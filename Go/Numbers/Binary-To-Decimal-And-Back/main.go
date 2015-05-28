ecdeeb8b9d50f7bf77daa2d53b50c772b3a39865
Derek Argueta <darguetap@gmail.com>
2015-05-27 22:35:52 -0600

    can convert binary to decimal in go

---
+++ Go/Numbers/Binary-To-Decimal-And-Back/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+// iterate through all the runes in the string and ensure they are either 1 or 0
+func validateBinaryArgument(binaryArg string) {
+	for _, char := range binaryArg {
+		if char != '0' && char != '1' {
+			fmt.Println("Invalid binary number")
+			os.Exit(1)
+		}
+	}
+}
+
+func validateDecimalArgument(decimalArg string) {
+	// TODO - validate all characters are 1-9
+}
+
+func convBinary2Decimal(num string) string {
+
+	// will catch if the string is invalid
+	validateBinaryArgument(num)
+
+	var totalDecNum int
+
+	for i, char := range num {
+		// grab 2**i
+		multiFactor := math.Exp2(float64(i))
+		// convert the char rune into an integer
+		charNumVal := int(char - '0')
+		// add to the total
+		totalDecNum += charNumVal * int(multiFactor)
+	}
+
+	return strconv.Itoa(totalDecNum)
+}
+
+func convDecimal2Binary(num string) string {
+
+	// will catch if the string is invalid
+	validateDecimalArgument(num)
+	return ""
+}
+
+func checkArgs() {
+	if len(os.Args) < 3 {
+		fmt.Println("Not enough arguments! Provide a method (bin|dec) and value")
+		os.Exit(1)
+	} else if len(os.Args) > 3 {
+		fmt.Println("Too many arguments!")
+		os.Exit(1)
+	}
+}
+
+func main() {
+
+	checkArgs()
+
+	var result string
+	if os.Args[1] == "bin" {
+		// convert last argument decimal -> binary
+		result = convDecimal2Binary(os.Args[2])
+	} else if os.Args[1] == "dec" {
+		// convert last argument binary -> decimal
+		result = convBinary2Decimal(os.Args[2])
+	} else {
+		fmt.Println("Bad method, please use `bin` or `dec`")
+		os.Exit(1)
+	}
+
+	fmt.Println("result: " + result)
+}
